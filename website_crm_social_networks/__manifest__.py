a556cb67cd2caeae24f55bced650d7647a8a37cf
Michael Brooks Guerra <mguerra.orsis@gmail.com>
2022-06-03 01:07:19 -0400

    [ADD] module: website_crm_social_networks

---
+++ website_crm_social_networks/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Website ",
+    "summary": """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+    "author": "Michael Brooks Guerra, Odoo Community Association (OCA)",
+    "website": "",
+    "category": "Website/Website",
+    "version": "13.0.0.0.1",
+    "license": "AGPL-3",
+    # any module necessary for this one to work correctly
+    "depends": ["website", "crm_social_networks"],
+    # always loaded
+    "data": ["data/website_data.xml", "views/inherited_website_partner_templates.xml"],
+}
---
+++ website_crm_social_networks/controllers/controllers.py
@@ -0,0 +1,48 @@
+from odoo import http
+from odoo.http import request
+
+customer_per_page = 20
+
+
+class WebsiteCrmSocialNetworks(http.Controller):
+    @http.route(
+        ["/my_customers", "/my_customers/page/<int:page>"],
+        type="http",
+        auth="public",
+        website=True,
+    )
+    def my_customers(self, page=0, **post):
+        Partner = request.env["res.partner"]
+        search_value = post.get("search")
+        domain = [("active", "=", True), ("assigned_partner_id", "!=", False)]
+        if search_value:
+            domain += [
+                "|",
+                "|",
+                ("name", "ilike", search_value),
+                ("facebook_account", "ilike", search_value),
+                "|",
+                ("twitter_account", "ilike", search_value),
+                ("linkedin_account", "ilike", search_value),
+            ]
+
+        # pager
+        url = "/my_customers"
+        partner_count = Partner.sudo().search_count(domain)
+        pager = request.website.pager(
+            url=url,
+            total=partner_count,
+            page=page,
+            step=customer_per_page,
+            scope=7,
+            url_args=post,
+        )
+        partners = Partner.sudo().search(
+            domain, offset=pager["offset"], limit=customer_per_page
+        )
+
+        values = {"partners": partners, "pager": pager, "post": post}
+
+        return request.render(
+            "website_crm_social_networks.partner_social_networks", values
+        )
